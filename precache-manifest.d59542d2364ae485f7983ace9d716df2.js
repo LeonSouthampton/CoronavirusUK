da7162eeb523aacafbaf8c07a8ae188c3b98b950
LeonSouthampton <61657117+LeonSouthampton@users.noreply.github.com>
2020-03-01 16:41:14 +0000

    Add files via upload

---
+++ precache-manifest.d59542d2364ae485f7983ace9d716df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89362862c5349b02c4329aeeff0b2111",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad02effb5cac58ecf66a",
+    "url": "./static/js/2.e26fc250.chunk.js"
+  },
+  {
+    "revision": "666f64aae48e482ae5db9d2fef2dd4a8",
+    "url": "./static/js/2.e26fc250.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d4d75eb9f74cee341ee",
+    "url": "./static/js/main.b769b529.chunk.js"
+  },
+  {
+    "revision": "ba74184a524a7e41ddf4",
+    "url": "./static/js/runtime-main.e80815b1.js"
+  }
+]);
